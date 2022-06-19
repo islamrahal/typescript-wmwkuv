a4d5032ddda83befdf8b3efd638ccaacfd6a113d
islamrahal <islam.rahal999@gmail.com>
2022-06-19 14:06:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-wmwkuv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-wmwkuv)
---
+++ islam/index.ts
@@ -0,0 +1,12 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+let num1: number;
+let num2: number;
+num1 = 123;
+num2 = 457;
+
+console.log("num1");
